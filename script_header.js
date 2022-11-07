8caee0ff7840e0d3a0ead9c3fb1988e80fa74a87
Liewie <lucascpbarbosa0@gmail.com>
2022-11-06 23:26:59 -0300

    .

--- script_header.js
+++ script_header.js
@@ -1,3 +1,7 @@
+const menu = document.getElementById('menu_hamburguer')
+const sombra_menu = document.querySelector('.sombra_menu_hamburguer')
+const div_pesquisa = document.querySelector('#div_pesquisa')
+
 function mudou(){
     if (window.innerWidth >= '604'){
         menu_hamburguer.style.display = 'none';
@@ -7,8 +11,7 @@ function mudou(){
     }
 }
 function abrirNav(){
-    let menu = document.getElementById('menu_hamburguer')
-    let sombra_menu = document.querySelector('.sombra_menu_hamburguer')
+    
 
     if (menu.style.display == 'inline-block' & sombra_menu.style.display == 'inline-block'){
         menu.style.display = 'none';
@@ -22,10 +25,18 @@ function abrirNav(){
 }
 
 function abrirPesquisa(){
-    if (div_pesquisa.style.display == 'block'){
-        div_pesquisa.style.display = 'none'
+
+    if (div_pesquisa.style.display == 'none'){
+        if(menu.style.display == 'inline-block' & sombra_menu.style.display == 'inline-block'){
+            div_pesquisa.style.display = 'inline-block'
+            div_pesquisa.style.width = '70%'
+        }
+
+        else{
+            div_pesquisa.style.display = 'inline-block'
+        }
     }
     else{
-        div_pesquisa.style.display = 'block';
+        div_pesquisa.style.display = 'inline-block';
     }
 }
