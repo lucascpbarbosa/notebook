d4a46e39badf500109a27727f42a92ca201bf6aa
Liewie <lucascpbarbosa0@gmail.com>
2022-11-07 17:33:11 -0300

    .

--- script_header.js
+++ script_header.js
@@ -3,16 +3,11 @@ const sombra_menu = document.querySelector('.sombra_menu_hamburguer')
 const div_pesquisa = document.querySelector('#div_pesquisa')
 
 function mudou(){
-    if (window.innerWidth >= '604'){
-        menu_hamburguer.style.display = 'none';
-    }
-    else{
-        menu_hamburguer.style.display = 'none'
+    if (window.innerWidth >= '650'){
+        div_pesquisa.style.display = 'none'
     }
 }
 function abrirNav(){
-    
-
     if (menu.style.display == 'inline-block' & sombra_menu.style.display == 'inline-block'){
         menu.style.display = 'none';
         sombra_menu.style.display = 'none'
@@ -20,23 +15,18 @@ function abrirNav(){
     else {
         menu.style.display = 'inline-block';
         sombra_menu.style.display = 'inline-block'
+        div_pesquisa.style.display = 'none'
 
     }
 }
 
 function abrirPesquisa(){
-
-    if (div_pesquisa.style.display == 'none'){
-        if(menu.style.display == 'inline-block' & sombra_menu.style.display == 'inline-block'){
-            div_pesquisa.style.display = 'inline-block'
-            div_pesquisa.style.width = '70%'
-        }
-
-        else{
-            div_pesquisa.style.display = 'inline-block'
-        }
+    if (div_pesquisa.style.display == 'inline-block'){
+        div_pesquisa.style.display = 'none'
     }
     else{
-        div_pesquisa.style.display = 'inline-block';
+        div_pesquisa.style.display = 'inline-block'
+        menu.style.display = 'none'
+        sombra_menu.style.display = 'none'
     }
 }
